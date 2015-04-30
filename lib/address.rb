b157707b23ffe7a3e3e0e822c07a460aa05dc403
David Abel & David Garber <student@epicodus.com>
2015-04-30 09:35:21 -0700

    Pass second test returns last name from an entry

--- lib/address.rb
+++ lib/address.rb
@@ -1,10 +1,15 @@
 class Address
 
-  define_method(:initialize) do |first_name|
+  define_method(:initialize) do |first_name, last_name|
     @first_name = first_name
+    @last_name = last_name
   end
 
   define_method(:first_name) do
     @first_name
+  end
+
+  define_method(:last_name) do
+    @last_name
   end
 end
--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -3,14 +3,14 @@ require('address')
 
   describe("#first_name") do
     it("returns the first name on an entry") do
-      test_address = Address.new("Bob")
+      test_address = Address.new("Bob", "Smith")
       expect(test_address.first_name()).to(eq("Bob"))
     end
   end
 
   describe("#last_name") do
-    it("returns the last name on an entry") dp
-      test_address = Address.new("Smith")
+    it("returns the last name on an entry") do
+      test_address = Address.new("Bob", "Smith")
       expect(test_address.last_name()).to(eq("Smith"))
     end
   end
