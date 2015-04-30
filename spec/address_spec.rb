2937ff908c57fd2898720d8791028dc8d7fab45d
David Abel & David Garber <student@epicodus.com>
2015-04-30 09:28:08 -0700

    returns the first name on an entry.creates first_method.rspec

--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -1,9 +1,16 @@
 require('rspec')
 require('address')
 
-describe("#first_name") do
-  it("returns the first name on an entry") do
-    test_address = Address.new("Bob")
-    expect(test_address.first_name()).to(eq("Bob"))
+  describe("#first_name") do
+    it("returns the first name on an entry") do
+      test_address = Address.new("Bob")
+      expect(test_address.first_name()).to(eq("Bob"))
+    end
   end
-end
+
+  describe("#last_name") do
+    it("returns the last name on an entry") dp
+      test_address = Address.new("Smith")
+      expect(test_address.last_name()).to(eq("Smith"))
+    end
+  end
