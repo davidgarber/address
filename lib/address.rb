66e94c7c0b238cb11bafbf0e947749a47143361f
David Abel & David Garber <student@epicodus.com>
2015-04-30 10:35:06 -0700

    uses hash on address_spec.rb, attr_reader on address.rb

--- lib/address.rb
+++ lib/address.rb
@@ -1,24 +1,26 @@
 class Address
 
+  attr_reader(:first_name, :last_name, :birth_month)
+
   @@all_address = []
 
-  define_method(:initialize) do |first_name, last_name, birth_month|
-    @first_name = first_name
-    @last_name = last_name
-    @birth_month = birth_month
+  define_method(:initialize) do |attributes|
+    @first_name = attributes.fetch(:first_name)
+    @last_name = attributes.fetch(:last_name)
+    @birth_month = attributes.fetch(:birth_month)
   end
 
-  define_method(:first_name) do
-    @first_name
-  end
-
-  define_method(:last_name) do
-    @last_name
-  end
-
-  define_method(:birth_month) do
-    @birth_month
-  end
+  # define_method(:first_name) do
+  #   @first_name
+  # end
+  #
+  # define_method(:last_name) do
+  #   @last_name
+  # end
+  #
+  # define_method(:birth_month) do
+  #   @birth_month
+  # end
 
   define_singleton_method(:all) do
     @@all_address
--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -8,21 +8,21 @@ require('address')
 
   describe("#first_name") do
     it("returns the first name on an entry") do
-      test_address = Address.new("Bob", "Smith", "July")
+      test_address = Address.new({:first_name => "Bob", :last_name => "Smith", :birth_month => "July"})
       expect(test_address.first_name()).to(eq("Bob"))
     end
   end
 
   describe("#last_name") do
     it("returns the last name on an entry") do
-      test_address = Address.new("Bob", "Smith", "July")
+      test_address = Address.new({:first_name => "Bob", :last_name => "Smith", :birth_month => "July"})
       expect(test_address.last_name()).to(eq("Smith"))
     end
   end
 
   describe("#birth_month") do
     it("returns the birth month on an entry") do
-      test_address = Address.new("Bob", "Smith", "July")
+      test_address = Address.new({:first_name => "Bob", :last_name => "Smith", :birth_month => "July"})
       expect(test_address.birth_month()).to(eq("July"))
     end
   end
@@ -35,7 +35,7 @@ require('address')
 
   describe("#save") do
     it("saves the entry") do
-      test_address = Address.new("Bob", "Smith", "July")
+      test_address = Address.new({:first_name => "Bob", :last_name => "Smith", :birth_month => "July"})
       test_address.save()
       expect(Address.all()).to(eq([test_address]))
     end
@@ -43,7 +43,7 @@ require('address')
 
   describe(".clear") do
     it("empties out all of the saved addresses") do
-      Address.new("Bob", "Smith", "July").save()
+      Address.new({:first_name => "Bob", :last_name => "Smith", :birth_month => "July"}).save()
       Address.clear()
       expect(Address.all()).to(eq([]))
     end
